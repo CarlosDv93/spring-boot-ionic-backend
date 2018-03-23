ecc331c7ee98ceff1c394be7103e49eb053d0aee
carlos.david <carlos9_cv@hotmail.com>
2018-03-23 09:44:11 -0300

    Criando o repository e o service de Categoria

---
+++ src/main/java/com/carlosdv93/udemy/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.carlosdv93.udemy.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.carlosdv93.udemy.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+	
+	
+}
