0ac72ec7ff9d7e10933783b48a3c1ad9748331fa
carlos.david <carlos9_cv@hotmail.com>
2018-05-11 13:20:52 -0300

    Item Pedido pt1

--- src/main/java/com/carlosdv93/udemy/cursomc/domain/Produto.java
+++ src/main/java/com/carlosdv93/udemy/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.carlosdv93.udemy.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -32,6 +34,9 @@ public class Produto implements Serializable{
 	)
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	private Set<ItemPedido> itens = new HashSet<>();
+
+	
 	public Produto() {
 		
 	}
@@ -41,6 +46,14 @@ public class Produto implements Serializable{
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
@@ -73,6 +86,14 @@ public class Produto implements Serializable{
 
 	public void setCategorias(List<Categoria> categoria) {
 		this.categorias = categoria;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
