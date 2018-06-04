869fee3ce0e82a2e7af5195eeaa0f330e8fb5591
carlos.david <carlos9_cv@hotmail.com>
2018-06-04 11:27:46 -0300

    Retirando reducancia ciclica para /pedidos/id

---
+++ src/main/java/com/carlosdv93/udemy/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.carlosdv93.udemy.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carlosdv93.udemy.cursomc.domain.Pedido;
+import com.carlosdv93.udemy.cursomc.repositories.PedidoRepository;
+import com.carlosdv93.udemy.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objecto não Encontrado: " + id + "\nTipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+	
+}
