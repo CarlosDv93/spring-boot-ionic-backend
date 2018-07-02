0729d00fdb55a166e3316dce14f5dc30d07c69f4
CarlosDv93 <carlos9_cv@hotmail.com>
2018-07-02 20:17:03 -0300

    PUT, GET, DELETE para Cliente

--- src/main/java/com/carlosdv93/udemy/cursomc/resources/ClienteResource.java
+++ src/main/java/com/carlosdv93/udemy/cursomc/resources/ClienteResource.java
@@ -1,13 +1,23 @@
 package com.carlosdv93.udemy.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carlosdv93.udemy.cursomc.domain.Cliente;
+import com.carlosdv93.udemy.cursomc.domain.Cliente;
+import com.carlosdv93.udemy.cursomc.dto.ClienteDTO;
 import com.carlosdv93.udemy.cursomc.services.ClienteService;
 
 @RestController
@@ -25,6 +35,43 @@ public class ClienteResource {
 		
 	}
 
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id){
+		Cliente obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		
+		List<Cliente> list = service.findAll();
+		/* Momento em que se usa a função stream do java 8 para mapear (Com o auxilio de uma função anonima (->) que recebe um objeto, e o converte em outro objeto */
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		
+		Page<Cliente> pageCliente = service.findPage(page, linesPerPage, orderBy, direction);
+		/* Como converter uma página para Página DTO usando o java 8 */
+		Page<ClienteDTO> pageDTO = pageCliente.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(pageDTO);
+		
+	}
 	
 	
 }
--- src/main/java/com/carlosdv93/udemy/cursomc/services/ClienteService.java
+++ src/main/java/com/carlosdv93/udemy/cursomc/services/ClienteService.java
@@ -1,10 +1,19 @@
 package com.carlosdv93.udemy.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.carlosdv93.udemy.cursomc.domain.Cliente;
+import com.carlosdv93.udemy.cursomc.domain.Cliente;
+import com.carlosdv93.udemy.cursomc.dto.ClienteDTO;
 import com.carlosdv93.udemy.cursomc.repositories.ClienteRepository;
+import com.carlosdv93.udemy.cursomc.services.exceptions.DataIntegrityException;
 import com.carlosdv93.udemy.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,6 +28,45 @@ public class ClienteService {
 			throw new ObjectNotFoundException("Objecto não Encontrado: " + id + "\nTipo: " + Cliente.class.getName());
 		}
 		return obj;
+	}
+	
+	public Cliente update(Cliente obj) {
+		
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		// TODO Auto-generated method stub
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+		
+		
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest);
+		
+	}
+	
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
 	}
 	
 }
