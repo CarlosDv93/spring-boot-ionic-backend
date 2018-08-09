38e737dacae4078e4973cf85fea9d14c35e9c2e9
CarlosDv93 <carlos9_cv@hotmail.com>
2018-08-09 10:46:03 -0300

    Validação Customizada: E-mail não repetido na inserção de cliente

--- src/main/java/com/carlosdv93/udemy/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/carlosdv93/udemy/cursomc/repositories/ClienteRepository.java
@@ -2,12 +2,15 @@ package com.carlosdv93.udemy.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.carlosdv93.udemy.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
-	
+	//padrão de nome do JPA
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 	
 }
