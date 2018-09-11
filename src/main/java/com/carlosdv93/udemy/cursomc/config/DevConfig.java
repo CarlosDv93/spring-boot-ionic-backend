08510aff166e2df7a4e1badc9155cdbaae652fc4
CarlosDv93 <carlos9_cv@hotmail.com>
2018-09-11 11:52:45 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/carlosdv93/udemy/cursomc/config/DevConfig.java
+++ src/main/java/com/carlosdv93/udemy/cursomc/config/DevConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.carlosdv93.udemy.cursomc.services.DBService;
+import com.carlosdv93.udemy.cursomc.services.EmailService;
+import com.carlosdv93.udemy.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -22,4 +24,9 @@ public class DevConfig {
 		return true;
 	}
 
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+	
 }
