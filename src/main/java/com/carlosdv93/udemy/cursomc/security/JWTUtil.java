c035d070629d2a5197472e7744856954c7cde55b
CarlosDv93 <carlos9_cv@hotmail.com>
2018-09-20 11:00:10 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/carlosdv93/udemy/cursomc/security/JWTUtil.java
@@ -0,0 +1,29 @@
+package com.carlosdv93.udemy.cursomc.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JWTUtil {
+
+	@Value("${jwt.secret}")
+	private String secret;
+	
+	@Value("${jwt.expiration}")
+	private long expiration;
+	
+	//Geração do Token de login
+	public String generateToken(String username) {
+		return Jwts.builder()
+				.setSubject(username)
+				.setExpiration(new Date(System.currentTimeMillis()+expiration))
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
+				.compact();
+	}
+	
+}
